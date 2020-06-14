71d066ea5dc58e8b331d2f63f6e3a4351d9b2a86
kemomoy <surumekarasu@gmail.com>
2020-06-14 18:44:31 +0900

    Made text input window

--- ankikun.py
+++ ankikun.py
@@ -1,12 +1,20 @@
 from kivy.app import App
 import japanize_kivy
+from kivy.uix.widget import Widget
+
+class MainScreen(Widget):
+    pass
+#    def __init__(self, **kwargs):
+#        super().__init__(**kwargs)
 
 class AnkikunApp(App):
 
     def __init__(self, **kwargs):
         super(AnkikunApp, self).__init__(**kwargs)
-        self.title = "英語暗記くん"
-        self.text = "こんにちは世界"
+#        self.title = "英語暗記くん"
+
+    def build(self):
+        return  MainScreen()
 
 if __name__ == "__main__":
     AnkikunApp().run()
