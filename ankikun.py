f45af3900a18ad336dde46122b0e2dae0648a91a
kemomoy <surumekarasu@gmail.com>
2020-06-15 09:57:53 +0900

    Add DB and data registration function

--- ankikun.py
+++ ankikun.py
@@ -1,11 +1,31 @@
 from kivy.app import App
 import japanize_kivy
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+import sqlite3
+conn = sqlite3.connect("example.sqlite3")
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS words(id int PRIMARY KEY, english text, translated text)''')
 
-class MainScreen(Widget):
-    pass
-#    def __init__(self, **kwargs):
-#        super().__init__(**kwargs)
+class ListScreen(Screen,Widget):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class MainScreen(Screen,Widget):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def wordlist_button(self):
+        self.clear_widgets()
+        return ListScreen()
+
+    def save_button(self):
+        c.execute('''insert into words(english, translated) values(:english, :translated)''',{"english":self.englishword.text,"translated":self.translatedword.text})
+        conn.commit()
+        for row in c.execute('''select * from words'''):
+            print(row)   
 
 class AnkikunApp(App):
 
@@ -14,7 +34,10 @@ class AnkikunApp(App):
 #        self.title = "英語暗記くん"
 
     def build(self):
-        return  MainScreen()
+        self.sm = ScreenManager()
+        self.sm.add_widget(MainScreen(name='main'))
+        self.sm.add_widget(ListScreen(name='sub'))
+        return self.sm
 
 if __name__ == "__main__":
     AnkikunApp().run()
